f56a3035a80e5d4a3171156f0880f420c4f794fd
KerelosNasser <keronaser2030@gmail.com>
2026-06-26 13:39:18 +0300

    feat(core): setup intl localization scaffolding

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../core/l10n/app_localizations.dart';
 import '../core/router/app_router.dart';
 import '../core/theme/app_theme.dart';
 
@@ -15,18 +16,16 @@ class WithJesusApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp.router(
-      title: 'مع يسوع',
       debugShowCheckedModeBanner: false,
       localizationsDelegates: const [
+        AppLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
-      supportedLocales: const [
-        Locale('ar'),
-        Locale('en'),
-      ],
+      supportedLocales: AppLocalizations.supportedLocales,
       locale: const Locale('ar'),
+      onGenerateTitle: (context) => AppLocalizations.of(context)!.appTitle,
       theme: AppTheme.themeData(AppThemeMode.light),
       darkTheme: AppTheme.themeData(AppThemeMode.dark),
       themeMode: ThemeMode.system,
